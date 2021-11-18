53d92bcb89d4404c0a0f6ab8bad0dd6d8f9f20f7
Simen Mailund Svendsen <simen.svendsen@ztlpay.io>
2021-11-18 20:05:39 +0100

    read json

--- external/wolt-api-bindings/domain/restaurants.go
+++ external/wolt-api-bindings/domain/restaurants.go
@@ -1,9 +1,24 @@
 package domain
 
-import "github.com/sMailund/woltergeist/external/wolt-api-bindings/entities"
+import (
+	"fmt"
+	"github.com/sMailund/woltergeist/external/wolt-api-bindings/entities"
+	"http"
+	"io"
+	"json"
+)
 
 func GetRestaurants() (*entities.Restaurants, error) {
-	r := new(entities.Restaurants)
+	var restaurants entities.Restaurants
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		// handle error
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
 
-	return r, nil
+	if err := json.Unmarshal(body, &restaurants); err != nil { // Parse []byte to the go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+	return &restaurants, nil
 }
